5a39e6d5dbd861fcd929038bc5189baae9e623c6
Maharaj Syed <maharajsyed@yahoo.com>
2020-01-12 01:25:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = ["x"," "," "," "," "," "," "," ","x"]
+board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
